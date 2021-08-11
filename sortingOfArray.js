f9a5dd4bb858087b237c1ca7ac0c9563272873f5
dev-qa <varopn@gmail.com>
2021-08-11 11:47:59 +0300

    1.1

--- sortingOfArray.js
+++ sortingOfArray.js
@@ -1,5 +1,11 @@
+function ascSorting(array) {
+  return array.sort(function(a,b){ return a - b});
+}
+
+function descSorting(array) {
+  return array.sort(function(a,b){ return b - a});
+}
+
 let testArray = [12, 4, 6, 89, 46, 100, -8];
-
-console.log(testArray.sort(function(a,b){ return a - b}));
-
-console.log(testArray.sort(function(a,b){ return b - a}));
+console.log(ascSorting(testArray));
+console.log(descSorting(testArray));
