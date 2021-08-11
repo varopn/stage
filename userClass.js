fe145b3f4a0cd753892f69726d06598bdb4e3556
dev-qa <varopn@gmail.com>
2021-08-11 11:12:51 +0300

    1.5

---
+++ userClass.js
@@ -0,0 +1,27 @@
+class User {
+    constructor(name,age,country) {
+        this._name = name;
+        this._age = age;
+        this._country = country
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    get country() {
+        return this._country;
+    }
+
+    getUserData(){
+        return `${this._name} from ${this._country} wit age ${this._age}`;
+    }
+};
+
+let user = new User('f',16,'g');
+console.log(user.getUserData());
+console.log(user.name);
