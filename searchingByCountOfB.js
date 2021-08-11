b2735d799788831571e141793671a39e945eba91
dev-qa <varopn@gmail.com>
2021-08-11 12:20:29 +0300

    1.4.3

--- searchingByCountOfB.js
+++ searchingByCountOfB.js
@@ -1,7 +1,9 @@
+function searchingByCountOfB(str) {
+    let regExp = /ab{2,4}a/g;
+
+    return str.match(regExp, '!b');
+}
+
 let str = 'aa aba abba abbba abbbba abbbbba';
 
-let regExp = /ab{2,4}a/g;
-
-let searchStr = str.match(regExp, '!b');
-
-console.log(searchStr);
+console.log(searchingByCountOfB(str));
