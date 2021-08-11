dfe9773f7b6aa05c8bf27b589bb9a73b395546ce
dev-qa <varopn@gmail.com>
2021-08-11 17:51:58 +0300

    Deleted additional files

--- JS_Tasks_By_Programm/searchingByCountOfB.js
+++
@@ -1,9 +0,0 @@
-function searchingByCountOfB(str) {
-    let regExp = /ab{2,4}a/g;
-
-    return str.match(regExp, '!b');
-}
-
-let str = 'aa aba abba abbba abbbba abbbbba';
-
-console.log(searchingByCountOfB(str));
--- JS_Tasks_By_Programm/searchingTripleA.js
+++
@@ -1,9 +0,0 @@
-function searchingTripleA(str) {
-    let regExp = /a{3}b/g;
-
-    return str.replace(regExp, '!b');
-}
-
-let str = 'aa aba abba abbba abbbba abbbbba aaab aab ab a abbba aaaab';
-
-console.log(searchingTripleA(str));
