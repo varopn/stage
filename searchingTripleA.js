47380f859b7a3e0bef3497382d5e49e3be960c88
dev-qa <varopn@gmail.com>
2021-08-11 12:18:25 +0300

    1.4.2

--- searchingTripleA.js
+++ searchingTripleA.js
@@ -1,7 +1,9 @@
+function searchingTripleA(str) {
+    let regExp = /a{3}b/g;
+
+    return str.replace(regExp, '!b');
+}
+
 let str = 'aa aba abba abbba abbbba abbbbba aaab aab ab a abbba aaaab';
 
-let regExp = /a{3}b/g;
-
-let searchStr = str.replace(regExp, '!b');
-
-console.log(searchStr);
+console.log(searchingTripleA(str));
