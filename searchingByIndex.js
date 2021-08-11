1de432ba16b542268241d721f3ddddffaf8c3eb0
dev-qa <varopn@gmail.com>
2021-08-11 12:54:27 +0300

    Deleted

--- searchingByIndex.js
+++
@@ -1,7 +0,0 @@
-let str = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit lest. Donec convallis dignissim ligula, et rutrum est elat vistibulum eu.';
-
-let regExp = /   /;
-
-let searchStr = str.match(regExp);
-
-console.log(searchStr);
