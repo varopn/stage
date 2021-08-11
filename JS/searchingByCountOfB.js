b8392f07811ca89475e08ab4cd9b2c1fc54120c1
dev-qa <varopn@gmail.com>
2021-08-11 12:24:20 +0300

    Removing additional directory

--- JS/searchingByCountOfB.js
+++
@@ -1,7 +0,0 @@
-let str = 'aa aba abba abbba abbbba abbbbba';
-
-let regExp = /ab{2,4}a/g;
-
-let searchStr = str.match(regExp, '!b');
-
-console.log(searchStr);
--- JS/sortingOfArray.js
+++
@@ -1,5 +0,0 @@
-let testArray = [12, 4, 6, 89, 46, 100, -8];
-
-console.log(testArray.sort(function(a,b){ return a - b}));
-
-console.log(testArray.sort(function(a,b){ return b - a}));
