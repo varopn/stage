f777a9437f83648a06eb91bf4870cfb1b93469fe
dev-qa <varopn@gmail.com>
2021-08-11 11:56:34 +0300

    1.2

--- minOfSubArrays.js
+++ minOfSubArrays.js
@@ -1,14 +1,19 @@
-let testArray = ([
-    [1, 5, 2, 6, 16, -1, 0, 12],
-    [2, 9, -3, -5, -57, 120, 12],
-    [14, -20, -30, 4, 0, 16, 150]
- ]);
- let sumOfMin = 0;
- let sumOfMax = 0;
+function sumOfMinDigitsInSubArrays(array) {
+   let sumOfMin = 0;
+   let sumOfMax = 0;
+  
+   for (let subArray of testArray) {
+      sumOfMin += Math.min(...subArray);
+      sumOfMax += Math.max(...subArray);
+   }
 
- for (let subArray of testArray) {
-    sumOfMin += Math.min(...subArray);
-    sumOfMax += Math.max(...subArray);
+   return `Sum of minimum values is [${sumOfMin}], sum of vaximum values is [${sumOfMax}]`;
  }
 
- console.log(sumOfMin,sumOfMax);
+let testArray = ([
+   [1, 5, 2, 6, 16, -1, 0, 12],
+   [2, 9, -3, -5, -57, 120, 12],
+   [14, -20, -30, 4, 0, 16, 150]
+]);
+
+console.log(sumOfMinDigitsInSubArrays(testArray));
