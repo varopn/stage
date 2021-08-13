409fde63c3448eec08cdc5d9e73470c223637012
dev-qa <varopn@gmail.com>
2021-08-13 11:37:14 +0300

    New realization of js tasks

--- JS_tasks_lite/sortingOfArray.js
+++ JS_tasks_lite/sortingOfArray.js
@@ -9,3 +9,19 @@ function descSorting(array) {
 let testArray = [12, 4, 6, 89, 46, 100, -8];
 console.log(ascSorting(testArray));
 console.log(descSorting(testArray));
+console.log(bubbleSort(testArray));
+
+function bubbleSort(arr) {
+  for(let j=0;j<arr.length;j++) {
+      for(let i = 0; i < arr.length; i++) {
+          if(arr[i]>arr[i+1]) {
+              let temp = arr[i];
+
+              arr[i] = arr[i+1];
+              arr[i+1] = temp;
+          }
+      }
+  }
+
+  return arr;
+}
