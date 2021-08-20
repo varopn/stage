f9df0b16ecbaae33a03eb679654882425bcb3f2e
dev-qa <varopn@gmail.com>
2021-08-20 18:09:54 +0300

    CRUD APP with Sequelize CLI EXPRESS

--- CRUD/routes/users.js
+++ CRUD/routes/users.js
@@ -29,43 +29,32 @@ catch(err) {
 });
 
 router.post("/create/users", async (req, res) => {
-  if (!req.body) {
-    res.status(400).send(({status: "No fully body"}));
-    console.log(req.body.name);
-  } else {
-    await services.userService.createNewUser({ 
-      name: req.body.name,
-      age: req.body.age,
-      additional_info: req.body.additional_info
-      }).then((user) => {
-        console.log(user);
-        if(!user) {
-          throw err;
-        };
-        res.redirect("/").status(201).send({user, status: "created"});
-      }).catch((err)=> { 
-        console.log(err);
-        return res.redirect("/").status(500).send({status: "Server error"});
-        });
+  await services.userService.createNewUser({ 
+    name: req.body.name,
+    age: req.body.age,
+    additional_info: req.body.additional_info
+  }).then((user) => {
+    console.log(user);
+    if(!user) {
+      throw err;
     };
+    res.redirect("/");
+  }).catch((err)=> { 
+      console.log(err);
+      return res.redirect("/");
+    });
 });
      
 router.get("/update/users/:id", async (req, res) => {
   await services.userService.getByUserId(req.params.id).then(async (user)=>{
     console.log(user);
-
-    if(!user) {
-      res.status(404).send({status: "No such user"});
-    } else {
-      console.log(user);
-      res.render("edit.hbs", {
-        user: user
+    res.render("edit.hbs", {
+      user: user
     });
-    };
   })
   .catch(err=>{
     console.log(err); 
-    return res.status(500).send({status: "Server error"});
+    return res.redirect("/");
   });
 });
   
@@ -75,32 +64,20 @@ router.post("/save/users/", async (req, res) => {
     age: req.body.age,
     additional_info: req.body.additional_info
   }, req.body.id).then(()=> {
-    res.redirect("/").status(200).send({status: "Successful updated"});
+    res.redirect("/");
   }).catch((err)=>{
     console.log(err);
-    return res.redirect("/").status(500).send({status: "Server error"});
+    return res.redirect("/");
   });
 });
 
 router.post("/delete/users/:id", async (req, res) => {
-  await services.userService.getByUserId(req.params.id).then(async (user)=>{
-    console.log(user);
-
-    if(!user) {
-      res.status(404).send({status: "No such user"});
-    } else {
-      await services.userService.deleteByUserId(req.params.id).then(()=>{
-        res.redirect("/").status(200).send({status: "Successful deleted"});
-      }).catch((err) => {
-        console.log(err);
-        return res.status(500).send({status: "Server error"}).redirect("/");
-      });
-    };
-  })
-  .catch((err)=>{
-    console.log(err);
-    return res.status(500).send({status: "Server error"});
-  });
+  await services.userService.deleteByUserId(req.params.id).then(()=>{
+    res.redirect("/");
+  }).catch((err) => {
+      console.log(err);
+      return res.redirect("/");
+    });
 });
 
 module.exports = router;
