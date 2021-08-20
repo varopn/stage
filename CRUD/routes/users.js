9e90ebbeb9515fdf7d0a6667f5c9e5417c2cfd69
dev-qa <varopn@gmail.com>
2021-08-20 16:33:01 +0300

    CRUD APP with Sequelize CLI EXPRESS FE

--- CRUD/routes/users.js
+++ CRUD/routes/users.js
@@ -6,107 +6,101 @@ const services = require('../services/');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get("/", (req, res) => {
-    res.json({ message: "Home page" });
-  });
-
-router.post("/users", async (req, res) => {
-    if (!req.body) {
-  
-      res.status(400).send(({status: "No fully body"}));
-      console.log(req.body.name);
-  
-    } else {
-  
-        await services.userService.createNewUser({
-  
-          name: req.body.name,
-          age: req.body.age,
-          additional_info: req.body.additional_info
-  
-        }).then((user)=>{
-  
-          console.log(user);
-          if(!user) {
-            res.status(500).send(({status: "Error"}));
-          }
-  
-          res.status(200).send({user, status: "created"});
-        }).catch(err=> { 
-  
-          console.log(err);
-  
-        });
-      };
-  });
-    
-router.get("/users", async (req, res) => {
-    await services.userService.getUsers().then(users=>{
-      console.log(users);
-  
-      if(users.length === 0) {
-        res.status(200).send({status: "we DON't HAVE USERS"});
-      }
-  
-      res.status(200).send(users);
-    }).catch(err=>console.log(err));
+router.get("/",async (req, res) => {
+  try {
+    let users = await services.userService.getUsers();
+    console.log(users);
+    res.render("index.hbs", {
+    users
+    });
+}
+catch(err) {
+    res.send(err);
+}
 });
-  
-router.put("/users/:id", async (req, res) => {
-  
-    await services.userService.updateByUserId({
-  
+
+router.get("/create",async (req, res) => {
+  try {
+    res.render("create.hbs");
+}
+catch(err) {
+    res.send(err);
+}
+});
+
+router.post("/create/users", async (req, res) => {
+  if (!req.body) {
+    res.status(400).send(({status: "No fully body"}));
+    console.log(req.body.name);
+  } else {
+    await services.userService.createNewUser({ 
       name: req.body.name,
       age: req.body.age,
       additional_info: req.body.additional_info
-  
-    }, req.params.id).then((user)=> {
-      if(!user) {
-        res.status(404).send({status: "User is not existing"});
-      }
-      res.status(200).send({user, status: "Successful updated"});
-    })
-    .catch((err)=>{
-      console.log(err);
-    });
-});
-  
-router.get("/users/:id", async (req, res) => {
-    await services.userService.getByUserId(req.params.id).then((user)=>{
-  
-      console.log(user);
-  
-      if(!user){
-  
-        res.status(404).send({status: "No such user"});
-  
-      } else
-  
-      res.status(200).send({user, status: "existing"});
-    })
-    .catch(err=>console.log(err));
-});
-  
-router.delete("/users/:id", async (req, res) => {
-    await services.userService.getByUserId(req.params.id).then(async (user)=>{
-  
-      console.log(user);
-  
-      if(!user) {
-        res.status(404).send({status: "No such user"});
-      }
-  
-      await services.userService.deleteByUserId(req.params.id).then(()=>{
-  
-        res.status(200).send({status: "Successful deleted"});
-  
-      }).catch(err => {
-  
+      }).then((user) => {
+        console.log(user);
+        if(!user) {
+          throw err;
+        };
+        res.redirect("/").status(201).send({user, status: "created"});
+      }).catch((err)=> { 
         console.log(err);
-  
-      });
-    })
-    .catch(err=>console.log(err));
+        return res.redirect("/").status(500).send({status: "Server error"});
+        });
+    };
+});
+     
+router.get("/update/users/:id", async (req, res) => {
+  await services.userService.getByUserId(req.params.id).then(async (user)=>{
+    console.log(user);
+
+    if(!user) {
+      res.status(404).send({status: "No such user"});
+    } else {
+      console.log(user);
+      res.render("edit.hbs", {
+        user: user
+    });
+    };
+  })
+  .catch(err=>{
+    console.log(err); 
+    return res.status(500).send({status: "Server error"});
   });
+});
+  
+router.post("/save/users/", async (req, res) => {
+  await services.userService.updateByUserId({
+    name: req.body.name,
+    age: req.body.age,
+    additional_info: req.body.additional_info
+  }, req.body.id).then(()=> {
+    res.redirect("/").status(200).send({status: "Successful updated"});
+  }).catch((err)=>{
+    console.log(err);
+    return res.redirect("/").status(500).send({status: "Server error"});
+  });
+});
+
+router.post("/delete/users/:id", async (req, res) => {
+  await services.userService.getByUserId(req.params.id).then(async (user)=>{
+    console.log(user);
+
+    if(!user) {
+      res.status(404).send({status: "No such user"});
+    } else {
+      await services.userService.deleteByUserId(req.params.id).then(()=>{
+        res.redirect("/").status(200).send({status: "Successful deleted"});
+      }).catch((err) => {
+        console.log(err);
+        return res.status(500).send({status: "Server error"}).redirect("/");
+      });
+    };
+  })
+  .catch((err)=>{
+    console.log(err);
+    return res.status(500).send({status: "Server error"});
+  });
+});
 
 module.exports = router;
