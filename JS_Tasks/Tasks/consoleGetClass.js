af9a27f8f52f26a233fbcc0a0baf6b624f2127ca
dev-qa <varopn@gmail.com>
2021-09-07 12:00:14 +0300

    1.0

--- JS_Tasks/Tasks/consoleGetClass.js
+++ JS_Tasks/Tasks/consoleGetClass.js
@@ -1,24 +1,12 @@
 class User {
   constructor(name, age, country) {
-    this._name = name;
-    this._age = age;
-    this._country = country;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  get age() {
-    return this._age;
-  }
-
-  get country() {
-    return this._country;
+    this.name = name;
+    this.age = age;
+    this.country = country;
   }
 
   getUserData() {
-    return `${this._name} from ${this._country} wit age ${this._age}`;
+    return `${this.name} from ${this.country} wit age ${this.age}`;
   }
 }
 
--- JS_Tasks/Tasks/userClass.js
+++ JS_Tasks/Tasks/userClass.js
@@ -1,36 +1,12 @@
 class User {
   constructor(name, age, country) {
-    this._name = name;
-    this._age = age;
-    this._country = country;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  get age() {
-    return this._age;
-  }
-
-  get country() {
-    return this._country;
-  }
-
-  set name(name) {
-    this._name = name;
-  }
-
-  set country(country) {
-    this._country = country;
-  }
-
-  set age(age) {
-    this._age = age + 1;
+    this.name = name;
+    this.age = age;
+    this.country = country;
   }
 
   getUserData() {
-    return `${this._name} from ${this._country} wit age ${this._age}`;
+    return `${this.name} from ${this.country} wit age ${this.age}`;
   }
 }
 
